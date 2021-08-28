44d96d8798f29880ec49ec5d8cfe88feab3297ab
Marcus Döberl <marcus.doberl@gmail.com>
2021-08-28 12:30:53 +0200

    Fixed snyggare events

--- src/components/fester/fester.js
+++ src/components/fester/fester.js
@@ -1,12 +1,26 @@
 import styled from 'styled-components'
 
-export const Fest = styled.h1`
-    color:  white;
-    background-color:   #754022;
-    border: pink;
-`;
-
-export const festinfo = styled.p`
-    color: white;
-    
-`;
+export const Title = styled.h1 `
+    font-size: 2rem;
+`
+export const Description = styled.p`
+    font-size:  1rem;
+`
+export const ÖMSpan = styled.span`
+    font-size:  1.5rem;
+`
+export const Row = styled.div`
+    display: flex;
+    padding-top:   2vh;
+`
+export const Col =  styled.div`
+    flex: ${(props) => props.size};
+    padding: 2vw;
+`
+export const Grid = styled.div`
+    background-color:   lightpink;
+    border-color:   brown;
+    border: 5px solid brown;
+    border-radius:  5%;
+    overflow:   hidden;
+`
---
+++ src/components/footer/FooterElements.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    color:  pink;
+    font-size: 2rem;
+`
+export const Description = styled.p`
+    font-size:  1rem;
+
+`
+export const Grid = styled.div`
+background-color: solid brown;
+
+`
+export const Row = styled.div`
+
+`
+export const Col = styled.div`
+
+`
