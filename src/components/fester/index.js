ae7eb846cc108178b53e804cd0756062ff3bf2e0
Marc Taylor <marc@taylorit.com>
2023-07-05 11:41:19 +0200

    upd some links, names and fix some misspellings

--- src/components/fester/index.js
+++ src/components/fester/index.js
@@ -73,10 +73,10 @@ function EventsGrid() {
           </Col>
           <Col size={1}>
             <Title>
-              D<ÖMSpan>ÖM</ÖMSpan>D 2022
+              D<ÖMSpan>ÖM</ÖMSpan>D 2023
             </Title>
             <Description>
-              Den 7e april slås dörrarna upp till årets bästa fest, DÖMD!
+              Den 11e april slås dörrarna upp till årets bästa fest, DÖMD!
               Datateknologsektionens Öppna Mästerskap i Dart är en av norra
               Europas största darttävlingar som arrangeras för 41:a året i rad.
               Festen som pågår i tre dagar och börjar med kravall på torsdagen,
