f8e2e91f015e60544b770c760eec008aeb08c2d5
Knivkalle <oliver@regnstrand.se>
2026-08-20 15:10:09 +0200

    Lägre upplösning på bilder

--- src/components/Groupies/index.js
+++ src/components/Groupies/index.js
@@ -4,63 +4,19 @@ import Grid from "@mui/material/Grid";
 import CardGrid from "./Card";
 import { Typography } from "@mui/material";
 
-// import embla from "../../images/dg2425/profiler/embla.jpg";
-// import emblaSmall from "../../images/dg2425/profiler/embla_426x640.jpg";
-// import tessSmall from "../../images/dg2425/profiler/tess_426x640.jpg";
-// import tess from "../../images/dg2425/profiler/tess.JPG";
-// import isak from "../../images/dg2425/profiler/isakbig.jpg";
-// import isakSmall from "../../images/dg2425/profiler/isakbig_426x640.jpg";
-// import jesper from "../../images/dg2425/profiler/jesper.jpg";
-// import jesperSmall from "../../images/dg2425/profiler/Jesper-4_426x640.jpg";
-// import jenny from "../../images/dg2425/profiler/jenny.jpg";
-// import jennySmall from "../../images/dg2425/profiler/jenny_426x640.jpg";
-// import elias from "../../images/dg2425/profiler/elias.jpg";
-// import eliasSmall from "../../images/dg2425/profiler/elias_426x640.jpg";
-// import melker from "../../images/dg2425/profiler/melker.jpg";
-// import melkerSmall from "../../images/dg2425/profiler/melker_426x640.jpg";
-// import axel from "../../images/dg2425/profiler/axel.jpg";
-// import axelSmall from "../../images/dg2425/profiler/axel_426x640.jpg";
-// import simon from "../../images/dg2425/profiler/simon.jpg";
-// import simonSmall from "../../images/dg2425/profiler/simon_426x640.jpg";
-// import alma from "../../images/dg2425/profiler/Alma.jpg";
-// import almaSmall from "../../images/dg2425/profiler/Alma_426x640.jpg";
-// import chrille from "../../images/dg2425/profiler/chrille.jpg";
-// import chrilleSmall from "../../images/dg2425/profiler/chrille_426x640.jpg";
-// import jonga from "../../images/dg2425/profiler/jonga.jpg";
-// import jongaSmall from "../../images/dg2425/profiler/jonga_426x640.jpg";
-// import olle from "../../images/dg2425/profiler/olle.jpg";
-// import olleSmall from "../../images/dg2425/profiler/olle_426x640.jpg";
-
-// SMALL behövs typ inte men kolla in på det om du är intresserad av att optimera laddningstider
-// import maxen from "../../images/dg2526/Profiler/maxmax.jpg";
-// import emelie from "../../images/dg2526/Profiler/emelie.jpg";
-// import klara from "../../images/dg2526/Profiler/klara.jpg";
-// import olle from "../../images/dg2526/Profiler/olle.jpg";
-// import hugo from "../../images/dg2526/Profiler/hugo.jpg";
-// import elin from "../../images/dg2526/Profiler/elin.jpg";
-// import gorbel from "../../images/dg2526/Profiler/gorbel.jpg";
-
-// // import gorbel from "../../images/dg2526/Profiler/gorbel2.0.jpg"
-
-// import qelb from "../../images/dg2526/Profiler/qelb.jpg";
-// import becky from "../../images/dg2526/Profiler/becky.jpg";
-// import jocke from "../../images/dg2526/Profiler/jocke.jpg";
-// import jacob from "../../images/dg2526/Profiler/jacob.jpg";
-// import kita from "../../images/dg2526/Profiler/kita.jpg";
-
-import oliver from "../../images/dg2627/profiler/Oliver.jpg";
-import nanna from "../../images/dg2627/profiler/Nanna.jpg";
-import gurr from "../../images/dg2627/profiler/Gustav.jpg";
-import burbel from "../../images/dg2627/profiler/Burbel.jpg";
-import melle from "../../images/dg2627/profiler/Melle.jpg";
-import pino from "../../images/dg2627/profiler/Pino.jpg";
-import sixten from "../../images/dg2627/profiler/Sixten.jpg";
-import snollo from "../../images/dg2627/profiler/Snollo.jpg";
-import ines from "../../images/dg2627/profiler/Ines.jpg";
-import sofie from "../../images/dg2627/profiler/Sofie.jpg";
-import totte from "../../images/dg2627/profiler/Totte.jpg";
-import stella from "../../images/dg2627/profiler/Stella.jpg";
-import felicia from "../../images/dg2627/profiler/Fel.jpg";
+import oliver from "../../images/dg2627/profiler/profiler-optimized/Oliver.jpg";
+import nanna from "../../images/dg2627/profiler/profiler-optimized/Nanna.jpg";
+import gurr from "../../images/dg2627/profiler/profiler-optimized/Gustav.jpg";
+import burbel from "../../images/dg2627/profiler/profiler-optimized/Burbel.jpg";
+import melle from "../../images/dg2627/profiler/profiler-optimized/Melle.jpg";
+import pino from "../../images/dg2627/profiler/profiler-optimized/Pino.jpg";
+import sixten from "../../images/dg2627/profiler/profiler-optimized/Sixten.jpg";
+import snollo from "../../images/dg2627/profiler/profiler-optimized/Snollo.jpg";
+import ines from "../../images/dg2627/profiler/profiler-optimized/Ines.jpg";
+import sofie from "../../images/dg2627/profiler/profiler-optimized/Sofie.jpg";
+import totte from "../../images/dg2627/profiler/profiler-optimized/Totte.jpg";
+import stella from "../../images/dg2627/profiler/profiler-optimized/Stella.jpg";
+import felicia from "../../images/dg2627/profiler/profiler-optimized/Fel.jpg";
 
 /* 
 Det som måste göras för att lägga tillbaka j'8 på hemsidan:
